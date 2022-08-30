abd7e743e909e70278bbf5d6388c483bace02adc
chaewoniiii <102212531+chaewone@users.noreply.github.com>
2022-08-30 17:53:08 +0900

    0830
    user/templates/header.html
    sho/urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from user.views import home
 
+
+
 urlpatterns = [
-    path('', home), # home 화면
+    path('', home, name='home'), # home 화면
     path('admin/', admin.site.urls),
     path('user/', include("user.urls")),
     path('product/', include("product.urls")),
